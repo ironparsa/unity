5cd84f73949d268da119832ed9501fe413b8cf43
ironparsa <ironparsa@gmail.com>
2019-02-22 01:25:35 -0500

    Movement script added to cube and camera is attached to cube
    Co-Authored-By: sydnieL <sydniel@users.noreply.github.com>

---
+++ GitHub/Assets/Scripts/movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    
+    public float movementForce = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * movementForce);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * movementForce);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * movementForce);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * movementForce);
+        }
+    }
+}
